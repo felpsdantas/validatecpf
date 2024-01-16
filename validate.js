c6481e2f42a8d9fbe2e9ca14620132685b809426
Felipe Dantas <111136828+felpsdantas@users.noreply.github.com>
2024-01-16 15:03:03 -0300

    Update validate.js

--- validate.js
+++ validate.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function validateCPF() {
         const cpf = cpfInput.value.replace(/\D/g, ''); // Remover caracteres não numéricos
-        fetch(`/api/validar_cpf/${cpf}`)
+        fetch(`http://127.0.0.1:5000/api/validar_cpf/${cpf}`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error(`Erro HTTP! Status: ${response.status}`);
