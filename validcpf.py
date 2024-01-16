b5e6db508be23192c386bbc7de76855f5775bfa4
Felipe Dantas <111136828+felpsdantas@users.noreply.github.com>
2024-01-16 15:03:30 -0300

    Update validcpf.py

--- validcpf.py
+++ validcpf.py
@@ -22,12 +22,12 @@ def valida_cpf(cpf):
     digito2 = 11 - (soma % 11) if soma % 11 >= 2 else 0
 
     # Verifica se os digitos verificadores estão corretos
-    return int(cpf[9]) == digito1 and int(cpf[10]) == digito2
+    return cpf[9:] == str(digito1 % 10) + str(digito2 % 10)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def pagina_inicial():
+    return 'Bem-vindo a minha pagina incial'
 
 @app.route('/api/validar_cpf/<cpf>', methods=['GET'])
 @cross_origin(supports_credentials=True, origins='*')
@@ -41,4 +41,4 @@ def validar_cpf_rota(cpf):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=8000)
+    app.run(debug=True)
